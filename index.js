68f1c48fd558989686d5dfd3e70afa4454e6059b
przemek <host9@o2.pl>
2018-05-18 23:36:00 +0200

    Steam passport log in

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const passport = require('passport');
 const keys = require('./config/keys');
+const SteamStrategy = require('passport-steam').Strategy;
 
 const app = express();
 
+passport.use(new SteamStrategy({
+        returnURL: 'http://localhost:5000/auth/steam/return',
+        realm: 'http://localhost:3000',
+        apiKey: keys.steamApiKey
+    },
+    function (identyfier, profile, done) {
+        User.findByOpenID({openId: identyfier}, function (err, user) {
+            return done(err, user)
+        });
+    }));
 
+app.get('/auth/steam', passport.authenticate('steam'), function (req, res) {
 
+});
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT);
-
+app.get('auth/steam/return', passport.authenticate('steam', {failureRedirect: '/login'}),
+    function (req, res) {
+        res.redirect('/');
+    });
