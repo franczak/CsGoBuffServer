0364f1710d903098d474a98559a2d837cd8996b9
przemek <host9@o2.pl>
2018-05-18 22:19:31 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const passport = require('passport');
+const keys = require('./config/keys');
+
+const app = express();
+
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
+
