f8691a41209446bb9661b215a8f8bdf010c47fe8
przemek <host9@o2.pl>
2018-05-20 19:15:01 +0200

    Add steam log in

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 const express = require('express');
-const passport = require('passport');
+const session = require('express-session');
 const keys = require('./config/keys');
-const SteamStrategy = require('passport-steam').Strategy;
+const steam = require('steam-login');
 
 const app = express();
 
-passport.use(new SteamStrategy({
-        returnURL: 'http://localhost:5000/auth/steam/return',
-        realm: 'http://localhost:3000',
-        apiKey: keys.steamApiKey
-    },
-    function (identyfier, profile, done) {
-        User.findByOpenID({openId: identyfier}, function (err, user) {
-            return done(err, user)
-        });
-    }));
+app.use(require('express-session')({ resave: false, saveUninitialized: false, secret: 'a secret' }));
+app.use(steam.middleware({
+    realm: 'http://localhost:5000/',
+    verify: 'http://localhost:5000/verify',
+    apiKey: keys.steamApiKey}
+));
 
-app.get('/auth/steam', passport.authenticate('steam'), function (req, res) {
-
+app.get('/', function(req, res) {
+    res.send(req.user == null ? 'not logged in' : 'hello ' + req.user.username).end();
 });
 
-app.get('auth/steam/return', passport.authenticate('steam', {failureRedirect: '/login'}),
-    function (req, res) {
-        res.redirect('/');
-    });
+app.get('/authenticate', steam.authenticate(), function(req, res) {
+    res.redirect('/');
+});
+
+app.get('/verify', steam.verify(), function(req, res) {
+    res.send(req.user).end();
+});
+
+app.get('/logout', steam.enforceLogin('/'), function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
+console.log("Listening 5000");
+
+
