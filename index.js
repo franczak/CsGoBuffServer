2467d4027043b06252f32ca0ebd41396fc02e78a
Jan Ignasik <ignasikj@gmail.com>
2018-05-22 22:53:18 +0200

    basic auth

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 const express = require('express');
 const session = require('express-session');
 const steam = require('steam-login');
+require('dotenv').config()
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors({credentials: true, origin: 'https://cs-go-buff-front.herokuapp.com/'}));
 app.use(require('express-session')({ resave: false, saveUninitialized: false, secret: 'a secret' }));
 app.use(steam.middleware({
-    realm: 'https://cs-go-buff-front.herokuapp.com/',
-    verify: 'https://cs-go-buff-front.herokuapp.com//verify',
+    realm: 'https://cs-go-buff.herokuapp.com/',
+    verify: 'https://cs-go-buff.herokuapp.com/auth/steam/callback',
     apiKey: process.env.steamApiKey}
 ));
 
 app.get('/', function(req, res) {
-    res.send(req.user == null ? 'not logged in' : 'hello ' + req.user.username).end();
+    req.user?
+        res.redirect('https://cs-go-buff-front.herokuapp.com/')
+        :
+        res.redirect('/auth/steam');
 });
 
-app.get('/authenticate', steam.authenticate(), function(req, res) {
+app.get('/auth/steam', steam.authenticate(), function(req, res) {
     res.redirect('/');
 });
 
-app.get('/verify', steam.verify(), function(req, res) {
-    res.send(req.user).end();
+app.get('/auth/steam/callback', steam.verify(), function(req, res) {
+    res.redirect('/');
+});
+
+app.get('/user', function(req, res) {
+    res.send(req.user);
 });
 
 app.get('/logout', steam.enforceLogin('/'), function(req, res) {
